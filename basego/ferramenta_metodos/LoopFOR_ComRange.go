2690983a496d2238ae593a65530c1828978f91a0
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-09-28 22:23:43 -0300

    okok

--- basego/ferramenta_metodos/LoopFOR_ComRange.go
+++ basego/ferramenta_metodos/LoopFOR_ComRange.go
@@ -11,15 +11,3 @@ func Execute_FOR_com_Range() {
 		// fmt.Println(indice, valor)
 	}
 }
-
-/*
-FOR_com_Range,
-conceito: só tem o for para iterar fazer loop no golang, a clausula range é para iterar/percorrer sobre iteraveis [string, map dicionario, array slice lista, ]
-
-sintaxe: for <variaveis de captura indice, valor> := range alvo { acao }
-
-passes_logicos: for variavelCaputuradora_INDICE, variavelCaputuradora_VALOR := range onde quero iterar { O QUE QUERO REPETIR usando as variaveis a cada rodagem do loop }
-
-obs: senão quer utilizar uma variavel substitua por _ anderline na captura, a cao que fizer com cada valor capturado será com efeitoColateral ou seja vai modificar o alvo original.
-
-*/
