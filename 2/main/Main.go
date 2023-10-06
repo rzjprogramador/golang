552744855c7d5046615426a2e4eeb52d1ffbda2a
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-10-06 18:11:49 -0300

    okok

--- 2/main/Main.go
+++ 2/main/Main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"github.com/rzj/main/fundamentos"
-	"github.com/rzj/main/fundamentos/pgm"
 )
 
 func main() {
 	println("Alo Mundo!")
 	fundamentos.IndexBase()
-	pgm.ExecuteLinguagem()
 }
